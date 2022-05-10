fb4ca83b99521ba442aedd21d1ab1b2671cc8c91
Scott Lind <scttlnd@gmail.com>
2022-05-10 14:48:43 +0200

    Added: text alignment, refactor: featured collection

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,20 +1,20 @@
 const featuredSwiper = new Swiper('.featured-swiper', {
-navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints: {
     // when window width is >= 320px
-    500: {
-        slidesPerView: 2
-    },
-    // when window width is >= 480px
-    768: {
-        slidesPerView: 2
-    },
-    // when window width is >= 640px
-    990: {
-        slidesPerView: 4
-    }
+        500: {
+            slidesPerView: 2
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 2
+        },
+        // when window width is >= 640px
+        990: {
+            slidesPerView: 4
+        }
     }
 });
