03910eda74450eab04e0a478688e846eed1d0fba
Scott Lind <scttlnd@gmail.com>
2022-06-18 13:45:38 +0200

    Before purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 gulp.task('sass', function() {
     return gulp.src('styles/*.scss')
             .pipe(sass())
             .pipe(gulp.dest('assets'))
 });
-
+gulp.task('purgecss', () => {
+    return gulp.src('styles/*.scss')
+        .pipe(purgecss({
+            content: ['sections/*.liquid', 'snippets/*.liquid']
+        }))
+        .pipe(gulp.dest('templates/customers'))
+});
 gulp.task('watch', function() {
-    gulp.watch('styles/**/*.scss', gulp.series('sass'));
+    gulp.watch('styles/**/*.scss', gulp.series('sass', 'purgecss'));
 });
